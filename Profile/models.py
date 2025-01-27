41f2d17b69715ee96e2d5d20e1bd994d7d10a1b0
Ali Ahmadi <aliahmadi79sh@gmail.com>
2025-01-27 21:29:22 +0330

    Initialize Base Configs & Create Models

--- Profile/models.py
+++ Profile/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class UserProfile(models.Model):
+    GENDER_CHOICES = [
+        ('male', 'Male'),
+        ('female', 'Female'),
+    ]
+
+    # Personal Information
+    first_name = models.CharField(max_length=255)  # نام (اجباری)
+    last_name = models.CharField(max_length=255)  # نام خانوادگی (اجباری)
+    national_code = models.CharField(max_length=10, unique=True)  # کد ملی (اجباری)
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES, blank=True, null=True)  # جنسیت (اختیاری)
+    passport_number = models.CharField(max_length=50, blank=True, null=True)  # شماره پاسپورت (اختیاری)
+    passport_expiry_date = models.DateField(blank=True, null=True)  # تاریخ انقضای پاسپورت (اختیاری)
+    is_foreign = models.BooleanField(default=False)  # خارجی هست یا نه (اختیاری)
+    date_of_birth = models.DateField(blank=True, null=True)  # تاریخ تولد (اختیاری)
+
+    # Contact Information
+    landline_number = models.CharField(max_length=15, blank=True, null=True)  # شماره تلفن ثابت (اختیاری)
+    mobile_number = models.CharField(max_length=15)  # شماره موبایل (اجباری)
+    address = models.TextField(blank=True, null=True)  # آدرس (اختیاری)
+
+    # Profile Picture
+    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)  # عکس پرسنلی (اختیاری)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class UserBankInfo(models.Model):
+    user = models.OneToOneField(UserProfile, on_delete=models.CASCADE, related_name='bank_info')  # ارتباط یک به یک با پروفایل کاربر
+
+    # Banking Information
+    card_number = models.CharField(max_length=16)  # شماره کارت (اجباری)
+    account_number = models.CharField(max_length=20, blank=True, null=True)  # شماره حساب (اختیاری)
+    iban = models.CharField(max_length=24, blank=True, null=True)  # شماره شبا (اختیاری)
+
+    def __str__(self):
+        return f"Bank Info for {self.user.national_code} {self.user.last_name} {self.card_number}"
--- ProfilesService/settings.py
+++ ProfilesService/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,12 +32,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'admin_persian',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Profile',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +79,12 @@ WSGI_APPLICATION = 'ProfilesService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'profileDB',
+        'USER': 'postgres',
+        'PASSWORD': '403723807',
+        'HOST': 'localhost',  # یا آدرس IP سرور پایگاه داده
+        'PORT': '',  # برای استفاده از پورت پیش‌فرض (5432) می‌توانید خالی بگذارید
     }
 }
 
@@ -104,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fa-ir'
 
 TIME_ZONE = 'UTC'
 
@@ -117,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_media/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
